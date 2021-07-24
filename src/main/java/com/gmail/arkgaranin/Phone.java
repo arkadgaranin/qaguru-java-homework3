a81c561f6c1135318f82e974e94a6eb221826b4d
Arkadiy <arkadiy.garanin@socialsys.ru>
2021-07-24 22:06:30 +0300

    Создал класс, описывающий объекты сущности Телефон

---
+++ src/main/java/com/gmail/arkgaranin/Phone.java
@@ -0,0 +1,56 @@
+package com.gmail.arkgaranin;
+
+public class Phone {
+
+  char type; // s-смартфон, b-кнопочный мобильный, a-аналоговый стационарный телефон
+  String color;
+  int price;
+  boolean isNew;
+
+  public Phone(boolean isNew) {
+    this.isNew = isNew;
+  }
+
+  void setType(char type) {
+    this.type = type;
+  }
+
+  void setColor(String color) {
+    this.color = color;
+  }
+
+  void setPrice(int price) {
+    this.price = price;
+  }
+
+  String getPhoneCondition() {
+
+    String PhoneCondition;
+
+    if (!isNew) {
+      PhoneCondition = "Поддержанный";
+    }
+    else {
+      PhoneCondition = "Новый";
+    }
+    return PhoneCondition;
+  }
+
+  public void phoneIdentification() {
+
+    String namePhone;
+
+    if (price <= 2000 & type == 'b') {
+      namePhone = "nokia6310";
+      System.out.println("По выбранным параметрам Ваш телефон соответствует: " + namePhone + "\n");
+    }
+    else if (price > 2000 & type == 's') {
+      namePhone = "iphone11";
+      System.out.println("По выбранным параметрам Ваш телефон соответствует: " + namePhone + "\n");
+    }
+    else {
+      System.out.println("К сожалению не получилось определить модель мобильного телефона :(");
+    }
+  }
+
+}
